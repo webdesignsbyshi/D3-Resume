ef5db186453972266413f5d2516a45eb2868df19
webdesignsbyshi <webdesignsbyshi@gmail.com>
2017-03-06 13:02:34 -0500

    dist folder in it

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -31,18 +31,18 @@ $('.skills-btns > a').click(function() {
 });
 
 /*Development Bar Graph*/
-var svg = d3.select("#dev"),
+var svg = d3.select('#dev'),
     margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom;
+    width = +svg.attr('width') - margin.left - margin.right,
+    height = +svg.attr('height') - margin.top - margin.bottom;
 
 var x = d3.scaleBand().rangeRound([0, width]).padding(0.2),
     y = d3.scaleLinear().rangeRound([height, 0]);
 
-var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var g = svg.append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-d3.tsv("devdata.tsv", function(d) {
+d3.tsv('devdata.tsv', function(d) {
   d.percent = +d.percent;
   return d;
 }, function(error, data) {
@@ -51,27 +51,27 @@ d3.tsv("devdata.tsv", function(d) {
   x.domain(data.map(function(d) { return d.name; }));
   y.domain([0, 100]);
 
-  g.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height + ")")
+  g.append('g')
+      .attr('class', 'axis axis--x')
+      .attr('transform', 'translate(0,' + height + ')')
       .call(d3.axisBottom(x));
 
-  g.append("g")
-      .attr("class", "axis axis--y")
+  g.append('g')
+      .attr('class', 'axis axis--y')
       .call(d3.axisLeft(y).ticks(10))
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("Percent");
+    .append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 6)
+      .attr('dy', '0.71em')
+      .attr('text-anchor', 'end')
+      .text('Percent');
 
-  g.selectAll(".bar")
+  g.selectAll('.bar')
     .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.name); })
-      .attr("y", function(d) { return y(d.percent); })
-      .attr("width", x.bandwidth())
-      .attr("height", function(d) { return height - y(d.percent); });
+    .enter().append('rect')
+      .attr('class', 'bar')
+      .attr('x', function(d) { return x(d.name); })
+      .attr('y', function(d) { return y(d.percent); })
+      .attr('width', x.bandwidth())
+      .attr('height', function(d) { return height - y(d.percent); });
 });
